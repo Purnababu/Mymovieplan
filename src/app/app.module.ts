230a32589f6839333dd76f9220e40d3177b58761
purna <p.purnababu@gmail.com>
2023-03-27 11:27:11 +0530

    first message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import{HttpClientModule} from '@angular/common/http';
+import { MoviesListComponent } from './movies-list/movies-list.component'
+import { MoviesService } from './movies.service';
+import { RegisterComponent } from './register/register.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movie/movie.component';
+import { MyBookingsComponent } from './my-bookings/my-bookings.component';
+import { LoginComponent } from './login/login.component';
+import { MovieFormComponent } from './movie-form/movie-form.component';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent,
+    RegisterComponent,
+    MoviesComponent,
+    MovieComponent,
+    MyBookingsComponent,
+    LoginComponent,
+    MovieFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [MoviesService],
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
